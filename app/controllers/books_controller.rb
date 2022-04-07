e8ca2825b9d21175466bfac51777d93b95a6feee
EC2 Default User <ec2-user@ip-172-31-22-189.ec2.internal>
2022-04-07 08:29:56 +0000

    [Add]エラー箇所修正 showのview作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,18 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
-  # def index
-    # @books = Book.all
-  # end
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
 
   def show
-    # @book = Book.find(params[id])
+    @book = Book.find(params[:id])
   end
 
   def edit
