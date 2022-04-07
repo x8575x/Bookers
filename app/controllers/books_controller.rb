3653ac94b8719069ad55039fc5648111de4641d5
EC2 Default User <ec2-user@ip-172-31-22-189.ec2.internal>
2022-04-07 07:15:27 +0000

    [Add]投稿機能追加 エラー発生

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,33 @@
 class BooksController < ApplicationController
-  def index
+
+  def new
+    @book = Book.new
   end
 
-  def show
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
-  
+
+  # def index
+    # @books = Book.all
+  # end
+
+  def show
+    # @book = Book.find(params[id])
+  end
+
   def edit
   end
-  
-  
+
+
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
