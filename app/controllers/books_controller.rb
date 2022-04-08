e2e5ac93f7a67f6c3f315be80cdb8246975dfe75
EC2 Default User <ec2-user@ip-172-31-22-189.ec2.internal>
2022-04-08 02:55:05 +0000

    [Add]link_toの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,9 +26,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book_path(book.id)
+  end
 
 
 private
